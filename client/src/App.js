3c50def92a5a6ecfff9a600021bfe98be3a89ba1
leahjwoodhead <leahjwoodhead@gmail.com>
2020-12-15 11:48:57 +0000

    set up

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import {Join, Chat} from './components'
+import Header from './components/Header'
 
 const App = () => (
+    <>
+    <Header/>
     <Router>
         <Route path='/' exact component={Join} />
         <Route path='/chat' component={Chat} />
     </Router>
+    </>
 )
 
 export default App
